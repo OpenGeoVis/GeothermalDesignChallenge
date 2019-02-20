7475c0149d56c9afae4fd2a219a0c9ed3ed6a97e
Bane Sullivan <banesullivan@gmail.com>
2019-02-20 16:24:30 -0700

    Update temp model stuff

--- gdc19/project.py
+++ gdc19/project.py
@@ -6,11 +6,16 @@ import PVGeo
 import gdc19
 
 
+def get_roi_bounds():
+    """The ROI"""
+    return [329924.98816, 344152.930125, 4252833.48213, 4270951.09811, -2700.0, 2300.0]
+
+
 def load_project():
     return omfvtk.load_project(gdc19.get_omf_project_filename())
 
 
-def load_kriged_temp():
+def load_kriged_temp(clip=True):
     """Load the kriged temperature model from Lane Boyd"""
     fkrig = gdc19.get_lane_path("geotherm-export/Geotherm_kriged_0.sgems")
     fvar = gdc19.get_lane_path("geotherm-export/Geotherm_kriged_0_krig_var.sgems")
@@ -22,4 +27,7 @@ def load_kriged_temp():
     # FROM SGeMS:
     grid.origin = (325000, 4.245e6, -2700)
     grid.spacing = (250, 250, 50)
+    if clip:
+        bounds = get_roi_bounds()
+        grid = grid.clip_box(bounds, invert=False)
     return grid

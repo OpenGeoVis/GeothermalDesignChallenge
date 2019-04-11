fd9236356db3ec5cdec2e217b64bfbed36c26801
Bane Sullivan <banesullivan@gmail.com>
2019-04-10 21:02:43 -0600

    Add data aggreagetion examples

---
+++ project/data-aggregation/gis.py
@@ -0,0 +1,60 @@
+"""
+
+GIS Data
+--------
+
+Convert all the GIS data to OMF
+
+Be sure to install ``gdal``::
+
+    conda install gdal
+
+"""
+
+
+import sys
+# This makes the gdc19 package avaialble
+sys.path.append('../../')
+import gdc19
+
+###############################################################################
+
+import vtki
+import omf
+import omfvtk
+import pandas as pd
+import numpy as np
+
+
+###############################################################################
+# First, get the land surface to match Z-coordinates of shape files with
+# the topography.
+
+description='vertices of meshed/interpolated surfaces of the ' \
+    'land surface (based on 10-meter DEM) used in the Phase 2B '\
+    'earth model. All data are georeferenced to UTM, zone 12N, '\
+    'NAD 83, NAVD 88.'
+
+land_surface = gdc19.surf_to_omf(
+    'land_surface_vertices.csv',
+    'land_surface',
+    description
+    )
+land_surface.validate()
+topo = omfvtk.wrap(land_surface)
+
+###############################################################################
+# Load the shape files
+shapes = gdc19.read_shape_file_to_omf(
+    gdc19.get_shp_path('FORGE_Outline'), topo_points=topo.points
+)
+shapes
+
+###############################################################################
+
+boundary = shapes[0]
+boundary.name = 'boundary'
+boundary.validate()
+
+###############################################################################
+omfvtk.wrap(boundary).plot(show_edges=False)
---
+++ project/data-aggregation/temperature.py
@@ -0,0 +1,53 @@
+"""
+Temperature Data
+----------------
+
+"""
+
+
+import sys
+# This makes the gdc19 package avaialble
+sys.path.append('../../')
+import gdc19
+
+###############################################################################
+import vtki
+import omf
+import omfvtk
+import pandas as pd
+import numpy as np
+
+
+###############################################################################
+
+
+
+# Load the temperature data
+_temp = pd.read_csv(
+            gdc19.get_temperature_path('well_based_temperature.csv')
+            )
+_temp.head()
+
+###############################################################################
+temperature = omf.PointSetElement(
+    name='temperature',
+    description='cumulative record of one-dimensional temperature modeling '\
+        'based off of well data. Temperature log data were exampled and '\
+        'extrapolated below the bottom of a number of wells. Temperatures '\
+        'are in degrees Celsius, and all location data are georeferenced to '
+        'UTM, zone 12N, NAD 83, NAVD 88.',
+    subtype='point',
+    geometry=omf.PointSetGeometry(
+        vertices=_temp[['x', 'y', 'z']].values
+    ),
+    data=[omf.ScalarData(
+        name='temperature (C)',
+        array=_temp['T'].values,
+        location='vertices'
+    ),]
+)
+temperature.validate()
+
+
+###############################################################################
+omfvtk.wrap(temperature).plot(show_edges=False)
